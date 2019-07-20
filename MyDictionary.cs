cb00734c970e73d3e1320399c0a4523dc9ced0d6
xu001 <2166226538@qq.com>
2019-07-20 13:30:51 +0800

    第一次提交list和dictionary的部分功能代码

---
+++ MyDictionary.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAndDic0710_1
+{
+    /// <summary>
+    /// 字典的底层实现，其实就是对List进行操作
+    /// </summary>
+    /// <typeparam name="K">键的泛型</typeparam>
+    /// <typeparam name="V">值的泛型</typeparam>
+    class MyDictionary<K, V>
+    {
+        MyList<K> keys;
+        MyList<V> values;
+
+        public MyDictionary()
+        {
+            keys = new MyList<K>(10);
+            values = new MyList<V>(10);
+        }
+        /// <summary>
+        /// 在键的list和值的list中对应下标的添加键和值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(K key, V value)
+        {            
+            if (ContainsKey(key))
+            {
+                throw new Exception("不能添加相同的键");
+            }
+            else
+            {
+                keys.Add(key);
+                values.Add(value);
+            }
+
+        }
+        /// <summary>
+        /// 字典中的键值对的数目
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return this.keys.Count;
+            }
+        }
+        /// <summary>
+        /// 清空字典中的键值对，但保留位置
+        /// </summary>
+        public void Clear()
+        {
+            this.keys.Clear();
+            this.values.Clear();
+        }
+        /// <summary>
+        /// 判断字典中是否含有指定的键
+        /// </summary>
+        /// <param name="key">指定的键</param>
+        /// <returns></returns>
+        public bool ContainsKey(K key)
+        {            
+            return this.keys.Contanis(key);
+        }
+        /// <summary>
+        /// 
+        /// 判断字典中是否含有指定的值
+        /// </summary>
+        /// <param name="value">指定的值</param>
+        /// <returns></returns>
+        public bool ContainsValue(V value)
+        {            
+            return this.values.Contanis(value);
+        }
+        /// <summary>
+        /// 移除指定键对应的键值对
+        /// </summary>
+        /// <param name="key">指定键</param>
+        /// <returns></returns>
+        public V Remove(K key)
+        {
+            if (ContainsKey(key))
+            {
+                this.keys.Remove(key);
+                this.values.RemoveAt(keys.IndexOf(key));
+                return this.values[keys.IndexOf(key)];
+            }
+            else
+            {
+                throw new Exception("不存在该键");
+            }
+        }
+        /// <summary>
+        /// 返回指定键的值  使用: 对象[key]
+        /// </summary>
+        /// <param name="key">指定键</param>
+        /// <returns></returns>        
+        public V this[K key]
+        {
+            get
+            {
+                if (ContainsKey(key))
+                {
+                    return this.values[keys.IndexOf(key)];
+                }
+                else
+                {
+                    throw new Exception("不存在此键");
+                }                
+            }
+            set
+            {
+                //可以将指定键位的值进行修改
+                this.values[keys.IndexOf(key)] = value;
+            }
+        }
+        /// <summary>
+        /// 返回字典中的值的集合
+        /// </summary>
+        /// <returns></returns>
+        public MyList<V> Values()
+        {           
+            return this.values;
+        }
+        /// <summary>
+        /// 返回字典中的键的集合
+        /// </summary>
+        /// <returns></returns>
+        public MyList<K> Keys()
+        {
+            return this.keys;
+        }
+
+    }
+}
---
+++ MyList.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAndDic0710_1
+{
+    class MyList<T>
+    {
+        private T[] arr;
+        private int myIndex = 0;//数组中实际含有元素的个数-1（代表最后一个元素的下标）
+        private int arrLen = 0;//数组的长度大小
+        public MyList(int leng = 10)
+        {
+            arr = new T[leng];
+            this.arrLen = leng;
+        }
+        /// <summary>
+        /// 往数组的myindex后面添加value
+        /// </summary>
+        /// <param name="value">添加的值</param>
+        public void Add(T value)
+        {
+            //判断是否有空位
+            if (this.Count == arrLen)
+            {
+                this.arrLen *= 2;
+                T[] newArr = new T[this.arrLen];
+                for(int i = 0; i < this.arr.Length; ++i)
+                {
+                    newArr[i] = arr[i];
+                }
+                newArr[myIndex] = value;
+                arr = newArr;
+            }
+            else
+            {
+                arr[myIndex] = value;
+            }
+            myIndex++;
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">添加的值</param>
+        /// <param name="index">要添加的下标</param>
+        public void Insert(T value, int index)
+        {
+            if (index > this.myIndex || index < 0)
+            {
+                throw new Exception("下标越界");
+            }
+            else if (index >= 0 && index <= this.myIndex)
+            {
+
+                if (this.Count >= this.arrLen)
+                {
+                    this.arrLen += 1;
+                }
+                T[] newArr = new T[this.Count + 1];
+                int i = 0;
+                for (; i < index; ++i)
+                {
+                    newArr[i] = this.arr[i];
+                }
+                newArr[i] = value;
+                for (; i <= this.myIndex; ++i)
+                {
+                    newArr[i + 1] = this.arr[i];
+                }
+                this.arr = newArr;
+                ++this.myIndex;                
+                
+            }
+        }
+        public T Get(int index)
+        {
+            return this.arr[index];
+        }
+        /// <summary>
+        /// 直接通过对象list[index]，就可以得到集合中的index位置的元素
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T this[int index]
+        {
+            get
+            {
+                return this.arr[index];
+            }
+            set
+            {
+                this.arr[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// 移除指定值
+        /// </summary>
+        /// <param name="value"></param>
+        public void Remove(T value)
+        {
+            RemoveAt(IndexOf(value));
+        }
+        /// <summary>
+        /// 移除指定下标的值
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            //如果index 大于最后数组元素下标
+            if (index > myIndex)
+            {
+                throw new Exception("下标越界");
+            }
+            else
+            {
+                myIndex--;
+                T[] newArr = new T[myIndex];
+                int i = 0;
+                for (; i < index; ++i)
+                {
+                    newArr[i] = arr[i];
+                }
+                for(; i <= myIndex; ++i)
+                {
+                    newArr[i]=arr[i+1];
+                }
+                arr = newArr;
+            }
+        }
+
+        /// <summary>
+        /// 清空数组的元素，指的是将原来的元素清空，但不是将位置也移除
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = 0; i < this.myIndex; ++i)
+            {
+                arr[i] = default(T);
+            }
+            this.myIndex = 0;
+        }
+        /// <summary>
+        /// 该数组含有元素的个数
+        /// </summary>
+        public int Count
+        {
+            get { return this.myIndex+1; }
+        }
+        /// <summary>
+        /// 从数组的第一个元素开始，查询指定值value
+        ///，如果找到就方法该值对应的下标，否则返回-1
+        /// </summary>
+        /// <param name="value">指定查找的值</param>
+        /// <returns></returns>
+        public int IndexOf(T value)
+        {
+            for(int i = 0; i <= myIndex; ++i)
+            {
+                if (arr[i].Equals(value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        /// <summary>
+        /// 从数组的最后一个元素开始往前查找，查到返回对应
+        /// 下标，否则返回-1
+        /// </summary>
+        /// <param name="value">指定查找的值</param>
+        /// <returns></returns>
+        public int LastIndexOf(T value)
+        {
+            for(int i = myIndex; i >= 0; --i)
+            {
+                if (arr[i].Equals(value))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        /// <summary>
+        /// 判断指定的值是否在数组中
+        /// </summary>
+        /// <param name="value">指定值</param>
+        /// <returns></returns>
+        public bool Contanis(T value)
+        {
+            for(int i = 0; i <= myIndex; ++i)
+            {
+                if (arr[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 将数组中的元素复制到另外一个数组中
+        /// </summary>
+        /// <returns></returns>
+        public T[] ToArray()
+        {
+            T[] newArr = new T[myIndex];
+            for(int i = 0; i <= myIndex; ++i)
+            {
+                newArr[i] = arr[i];
+            }
+            return newArr;
+        }
+    }
+}
